6f8ec130d5d5ab77ee20f4f8dce1813726089cbe
Hasan Aktaş <aktashasan1130@gmail.com>
2022-11-16 10:40:39 +0300

    patika-store completed

---
+++ src/main/java/PatikaStoreApplication.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PatikaStoreApplication {
+    public static void main(String[] args) {
+
+        Store patikaStore = new Store();
+        patikaStore.run();
+    }
+}
